1f60c29125530d60c6695ce430141f1e5a28acf9
Qianwen <qianwen.xie.us@gmail.com>
2018-02-24 21:13:37 -0800

    Todo component

---
+++ react-self-app/src/component/Todo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '.././App.css';
+
+class Todo extends Component {
+  render() {
+    return (
+      <div className="Todo">
+        Hello World !
+      </div>
+    );
+  }
+}
+
+export default Todo;
